847fdbeb3c9044cfacdfb6ad74ec4e47eab872e0
Fydre <andriyhermann@gmail.com>
2021-07-03 17:44:40 +0200

    n

--- archive.php
+++ archive.php
@@ -31,18 +31,104 @@
       </div>
     </header>
     <div class="main archive">
-      <div class="sectionList">
-        <div class="leftMenu">
+      <div class="filteroptions">
+          <div class="numfilter">
+            <div class="numcontainer">
+              <span>All Branches</span>
+              <!-- insert Icon here -->
+            </div>
+            <div class="nums">
+              <div class="numcontainer">
+                <h3>check-in</h3>
+                <div class="date">
+                  <em>26</em>
+                  <div class="monthselect">
+                    <span>Jun</span>
+                    <!-- insert Icon here -->
+                  </div>
+                </div>
+              </div>
+              <div class="numcontainer">
+                <h3>check-out</h3>
+                <div class="date">
+                  <em>28</em>
+                  <div class="monthselect">
+                    <span>Jun</span>
+                    <!-- insert Icon here -->
+                  </div>
+                </div>
+              </div>
+              <div class="numcontainer">
+                <h3>guests</h3>
+                <em>1</em>
+                <div class="updownchevron">
 
-        </div>
-        <div class="container">
-          <div class="numberInfo">
-
+                </div>
+              </div>
+              <div class="numcontainer">
+                <h3>nights</h3>
+                <em>2</em>
+              </div>
+            </div>
           </div>
+          <div class="servicefilter">
+            <h3>Services</h3>
+            <form class="tickoptions">
+              <input type="checkbox" name="" value=""><span>Animal Allowed</span>
+              <input type="checkbox" name="" value=""><span>Animal Allowed</span>
+              <input type="checkbox" name="" value=""><span>Animal Allowed</span>
+              <input type="checkbox" name="" value=""><span>Animal Allowed</span>
+              <input type="checkbox" name="" value=""><span>Animal Allowed</span>
+              <input type="checkbox" name="" value=""><span>Animal Allowed</span>
+            </form>
+          </div>
+          <div class="sizefilter closed">
+            <h3>Room Size</h3>
+          </div>
+          <div class="extrasvcfilter closed">
+            <h3>Extra Services</h3>
+          </div>
+          <div class="numreview">
+            <h3>Reviews</h3>
 
-        </div>
+            <?php
+              for ($i=0; ; $i+=2) {
+                if ($i > 10) {
+                  break;
+                };
+                print_r("
+                <div class=\"numcontainer\">
+                  <h3>{$i}</h3>
+                </div>
+                ");
+              }
+            ?>
+          </div>
+          <div class="starreview">
+            <h3>Branch Stars</h3>
+          </div>
+      </div>
+      <div class="cardList">
+        <?php
+          for ($i=0; $i < 5; $i++) {
+            echo "<div class=\"card\">
+              <div class=\"img\">
+                <img src=\"\" alt=\"\">
+              </div>
+              <div class=\"text\">
+                <h4>Family Room</h4>
+                <div class=\"roomratings\">
+                  <span>5 Reviews</span><span>3 Guests</span><span>25M²</span>
+                </div>
+                <span>hfbluifwjehnchduheuieowueijrrelregiuujjuhfffuuujuujghhghgghgggggggdbcjöpcbuidbfpvjbjdsdpifsbföupcndjjfcbduödnvejawuibvdbaevbfdpbvcibdcbasuidibcasduibfcudsabfiodsubafudasibasuibsuoidbiuf</span>
+                <button class=\"read\">Book for XXX$</button>
+              </div>
+            </div>";
+          };
+         ?>
       </div>
     </div>
+
   <?php
     include "component/footer.php"
    ?>
